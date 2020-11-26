95ad98114153bd532e2c3a86ba088019ada0df1d
Jhonatas Flor de Sousa <58887452+FlorSousa@users.noreply.github.com>
2020-11-26 18:58:04 -0300

    Main.py criado
    Arquivo main.py que define o menu e importa

---
+++ main.py
@@ -0,0 +1,11 @@
+#from ChaveA import*
+#from ChaveB import*
+#from ChaveC import*
+
+rodando = True
+while(rodando):
+    print("CHAVE DE IDENTIFICAÇÃO")
+    
+    chave=input("A. flores aclamídeas ou monoclamídeas......CHAVE A\nB. UHWDWHI.......CHAVE B\nC. JFSDKJFAKF......CHAVE C\nEscolha a chave: ")
+    if chave=="A":
+        encontraFamíliaChaveA():
